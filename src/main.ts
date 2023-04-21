8e2117ab5f2935ca8dc3965ab3a44bde6ecb212c
Alexander Zwisler <alex-z-@web.de>
2023-04-21 12:06:01 +0200

    fix: wrong url

--- src/main.ts
+++ src/main.ts
@@ -5,10 +5,14 @@ import {OpenAPI as CoreOpenApi} from "./api";
 import {OpenAPI as AuthOpenApi} from "./auth-api";
 import {authCodeFlowConfig} from "./app/auth.config";
 
+function prepareUrl(url: string) {
+  return url.endsWith('/') ? url.substring(0, url.length - 1) : url;
+}
+
 (async () => {
   const config = await fetch('config.json').then(res => res.json());
-  CoreOpenApi.BASE = config.CORE_URL;
-  AuthOpenApi.BASE = config.AUTH_URL;
+  CoreOpenApi.BASE = prepareUrl(config.CORE_URL);
+  AuthOpenApi.BASE = prepareUrl(config.AUTH_URL);
   authCodeFlowConfig.issuer = config.AUTH_URL;
   platformBrowserDynamic().bootstrapModule(AppModule)
     .catch(err => console.error(err));
